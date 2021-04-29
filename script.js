771e91d2db8c577c9d94cdbb24b57c85a88e48e7
aadityadeshmukh <aadityadeshmukh@gmail.com>
2021-04-29 17:03:23 +0530

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('hello!')
+//Adding a 16x16 square grid
+//Create a base div
+baseDiv = document.createElement("div");
+baseDiv.setAttribute("id", "container");
+document.body.append(baseDiv);
+//run a loop for creating a 16x16 matrix
+for (i = 0; i < 16; i++) {
+  rowDiv = document.createElement("div");
+  rowDiv.setAttribute("id", `row${i + 1}`);
+  rowDiv.setAttribute("class", "row");
+  for (j = 0; j < 16; j++) {
+    divElem = document.createElement("div");
+    divElem.setAttribute("id", `${j + 1}`);
+    divElem.setAttribute("class", "column");
+    rowDiv.append(divElem);
+  }
+  baseDiv.append(rowDiv);
+}
